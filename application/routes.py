ec31b897c0ce622c472cd55a6b2ce330a0bb67cb
Quan Le <binht1977@gmail.com>
2026-01-26 14:04:50 +0000

    finished section 2.4 Create Nave Links

--- application/routes.py
+++ application/routes.py
@@ -3,6 +3,25 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',login=True)
+
+@app.route('/course_offerings')
+def course_offerings():
+    return render_template('course_offerings.html')             
+
+@app.route('/register')
+def register():           
+    return render_template('register.html')     
+
+@app.route('/login')
+def login():           
+    return render_template('login.html')            
+
+@app.route('/logout')
+def logout():           
+    return render_template('logout.html')           
+
+
 
