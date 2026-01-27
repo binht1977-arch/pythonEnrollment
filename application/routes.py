905490ce4e7bf9e0cf2466b56bd31e539d9e1426
grovebury <62478079+grovebury@users.noreply.github.com>
2026-01-27 16:07:15 +0000

    3 4 request response (#5)
    * updated
    
    * completed POST changes
    
    * completed post method changs section 3 comleted.
    
    ---------
    
    Co-authored-by: Quan Le <binht1977@gmail.com>

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,8 @@
 from application import app 
 from flask import render_template
+from flask import request, json, Response
+
+courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
 
 @app.route('/')
 @app.route('/index')
@@ -15,16 +18,31 @@ def login():
 @app.route('/courses/')
 @app.route("/courses/<term>")
 def courses(term="Spring 2019"):  #example test http://127.0.0.1:5000/courses/Fall%202020
-    courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
-
-    print(courseData)
-    print(courseData[0]['title']) # print the first title example
     return render_template('courses.html', courseData=courseData, courses=True, term=term)
 
 
 @app.route('/register')
 def register():           
     return render_template('register.html', register=True)     
+
+@app.route('/enrollment', methods=['get', 'post'])
+def enrollment():      
+    id = request.form.get('courseID')
+    title = request.form['title'] # same as get method above
+    term = request.form.get('term')
+
+    return render_template('enrollment.html', enrollment=True, data={'courseID': id, 'title': title, 'term': term})          
+
+@app.route('/api/')       #http://127.0.0.1:5000/api          -- test example
+@app.route('/api/<idx>')  #http://127.0.0.1:5000/api/0        -- test example
+def api(idx=None):           
+    if idx is None:
+        jdata = courseData
+    else:
+        jdata = courseData[int(idx)]
+
+    return Response(json.dumps(jdata), mimetype="application/json")
+
 
 @app.route('/logout')
 def logout():           
