ef94e69f461f7fae875669f9fae91703a4fff855
grovebury <62478079+grovebury@users.noreply.github.com>
2026-01-30 11:23:56 +0000

    5 working web forms (#7)
    * completed upto 4.3 connecting to the database
    
    * completed working with databases
    
    * completed up to Flashing Messages
    
    * completed Displaying form validation error messages
    
    * Completed section 4: Processing form data and updating the database
    
    * completed Updating registration route to interact with database
    
    * 5 Creating the MongoDB aggregation pipeline completed.
    
    * completed section 5
    
    ---------
    
    Co-authored-by: Quan Le <binht1977@gmail.com>

---
+++ application/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from application.models import User 
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(),Length(min=6, max=15)])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Login")
+
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(),Email()])
+    password = PasswordField("Password", validators=[DataRequired(),Length(min=6, max=30)]) # use PasswordField to mask input
+    password_confirmed = PasswordField("Confirm Password", validators=[DataRequired(),Length(min=6,max=30),EqualTo('password')])
+    first_name = StringField("First Name", validators=[DataRequired(), Length(min=1, max=55)])
+    last_name = StringField("Last Name", validators=[DataRequired(),Length(min=1,max=55)])
+    submit = SubmitField("Register Now")
+
+    def validate_email(self,email):
+        user = User.objects(email=email.data).first()  # query mongoengine for existing email
+        #user_all = User.objects(email=email.data).all()
+        #print('User: ',user,'/n')
+        #print('user_all: ',user, '/n')
+        if user:
+            raise ValidationError("Email is already in use, pick another one.")
+        
+
+    
